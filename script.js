4aba1b8b2eb951696e810599f0119aff2f719e88
Coco <coco.li7777@gmail.com>
2020-03-29 12:42:08 +1100

    update line chart of new cases

--- script.js
+++ script.js
@@ -12,7 +12,7 @@
     var rawData = await callRemoteApi(endpoint);
     console.log(rawData.locations);
     drawTotolPieChart(rawData.locations, 'latestUpdate', 'confirmed');
-    drawLineChartByStates(
+    drawLineChartByNewCase(
       rawData.locations,
       'newCase',
       'confirmed',
@@ -35,6 +35,34 @@
 
     loader.style.display = 'none';
   }
+  function mapState(state) {
+    switch (state) {
+      case 'Australian Capital Territory':
+        return 'ACT';
+        break;
+      case 'New South Wales':
+        return 'NSW';
+        break;
+      case 'Northern Territory':
+        return 'NT';
+        break;
+      case 'South Australia':
+        return 'SA';
+        break;
+      case 'Tasmania':
+        return 'TAS';
+        break;
+      case 'Victoria':
+        return 'VIC';
+        break;
+      case 'Western Australia':
+        return 'WA';
+        break;
+      case 'Queensland':
+        return 'QLD';
+        break;
+    }
+  }
 
   function mapStateData(stateData, key) {
     var allDates = Object.keys(stateData.timelines[key].timeline);
@@ -44,25 +72,21 @@
       .map(value => {
         return value.split('T')[0];
       });
-    //var newCaseNum =
+
     var lastTenDaysValues = allDatesValues.slice(
       allDatesValues.length - 10,
       allDatesValues.length - 1
     );
     var lastTenDaysNewCaseValues = lastTenDaysValues.map((item, index, arr) => {
-      if (index > arr.length - 1) {
-        return;
-      } else {
-        return arr[index + 1] - item;
-      }
+      return item - arr[index - 1];
     });
-    console.log(lastTenDaysNewCaseValues);
+    //console.log(lastTenDaysNewCaseValues.slice(1, 8));
 
     return {
       days: lastTenDays,
       values: lastTenDaysValues,
-      newCaseNum: lastTenDaysNewCaseValues.slice(0, 7),
-      title: stateData.province,
+      newCaseNum: lastTenDaysNewCaseValues.slice(1, 9),
+      title: mapState(stateData.province),
       num: stateData.latest[key],
       confirm: stateData.latest[key]
     };
@@ -99,9 +123,49 @@
       title: {
         display: true,
         text: 'Confirmed Number'
+      },
+      legend: {
+        position: 'left',
+        align: 'start'
       }
     };
     renderChart(ctx, 'doughnut', data, options);
+  }
+
+  function drawLineChartByNewCase(rawData, containerId, category, title) {
+    var ctx = document.getElementById(containerId).getContext('2d');
+    var formatedData = rawData.map(value => {
+      return mapStateData(value, category);
+    });
+    var data = {
+      labels: formatedData[0].days,
+      datasets: formatedData.map(state => {
+        return {
+          label: `${state.title}: ${
+            state.newCaseNum[state.newCaseNum.length - 1]
+          }`,
+          fill: false,
+          data: state.newCaseNum
+        };
+      }),
+      backgroundColor: [
+        '#6c5ce7',
+        '#ffcd56',
+        '#e17055',
+        '#00b894',
+        '#d63031',
+        '#00cec9',
+        '#74b9ff',
+        '#e84393'
+      ]
+    };
+    var options = {
+      title: {
+        display: true,
+        text: title
+      }
+    };
+    renderChart(ctx, 'line', data, options);
   }
 
   function drawLineChartByStates(rawData, containerId, category, title) {
